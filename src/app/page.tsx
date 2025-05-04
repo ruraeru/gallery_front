b9a070c2062144d24c2aca8c48c0a6c26827f396
ruraeru <f93335383@gmail.com>
2025-05-04 23:08:16 +0900

    design: main img layout design & header, footer design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,27 +7,93 @@ export default async function Home() {
   const getCahcedImages = nextCache(getImages, ["images"]);
   const images = await getCahcedImages();
   return (
-    <div>
-      <div style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
-        gap: "32px",
-        placeItems: "center"
+    <div style={{
+
+    }}>
+      <header style={{
+        height: "80px",
+        backgroundColor: "gray",
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "16px",
       }}>
-        {images.map((image) => (
-          <Link href={`/photo/${image.id}`} key={image.id}>
-            <div key={image.id} style={{
-              position: "relative",
-              placeItems: "center center"
-            }}>
-              <Image style={{
-                objectFit: "cover",
-                margin: "0 auto",
-              }} src={image.urls.regular} width={160} height={240} priority alt="s" />
-            </div>
-          </Link>
-        ))}
-      </div>
+        <div>
+          menu
+        </div>
+        <div>
+          logo
+        </div>
+        <div style={{
+          display: "flex", //헤더 디자인 중이었음 ㅇㅇ
+          gap: "6px"
+        }}>
+          <div>
+            Links
+          </div>
+          <div>
+            login status
+          </div>
+        </div>
+      </header>
+
+      <main style={{
+        minHeight: "100vh"
+      }}>
+        {/* Banner top3 images*/}
+        <div style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}>
+          <div style={{
+            display: "flex",
+          }}>
+            {images.slice(0, 3).map((image) => {
+              return (
+                <Link href={`/photo/${image.id}`} key={image.id}>
+                  <div key={image.id} style={{
+                    position: "relative",
+                  }}>
+                    <Image style={{
+                      objectFit: "cover",
+                    }} src={image.urls.raw} width={480} height={480} priority alt="s" />
+                  </div>
+                </Link>
+              )
+            })}
+          </div>
+        </div>
+
+        {/* image list */}
+        <div style={{
+          display: "flex",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          padding: "32px",
+          gap: "16px",
+          height: "240px"
+        }}>
+          {images.map((image) => (
+            <Link href={`/photo/${image.id}`} key={image.id}>
+              <div key={image.id} style={{
+                position: "relative",
+              }}>
+                <Image style={{
+                  objectFit: "cover",
+                }} src={image.urls.regular} width={195} height={240} priority alt={image.alt_description} />
+              </div>
+            </Link>
+          ))}
+        </div>
+      </main>
+      <footer style={{
+        height: "80px",
+        backgroundColor: "gray",
+        width: "100%",
+      }}>
+        sddsa
+      </footer>
     </div>
   );
 }
