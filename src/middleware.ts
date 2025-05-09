dcd6d0689d60dfb8c912c0a3314b0eaec1bd5849
ruraeru <f93335383@gmail.com>
2025-05-10 00:15:12 +0900

    feat: middle ware

---
+++ src/middleware.ts
@@ -0,0 +1,37 @@
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/session";
+
+interface Routes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/signup": true,
+};
+
+//middleware === edge funtion
+//빠르게 실행되야하고 경량화 NodeJS API라 지원하지 않는 패키지들이 많음
+//https://nextjs.org/docs/pages/api-reference/edge
+export async function middleware(req: NextRequest) {
+  const session = await getSession();
+  const exists = publicOnlyUrls[req.nextUrl.pathname];
+  if (!session.id) {
+    if (!exists) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  } else {
+    // if (exists) {
+    //   return NextResponse.redirect(new URL("/products", req.url));
+    // }
+  }
+  // const cookie = await cookies();
+  // session.id === cookie.get("delicious-karrot");
+}
+
+//middleware가 실행 될 곳을 지정 가능
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
