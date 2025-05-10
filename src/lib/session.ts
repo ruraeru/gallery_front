8acc015741a2816e1cc86a100e39fbf2c2c6ab15
ruraeru <f93335383@gmail.com>
2025-05-10 15:14:08 +0900

    build: 빌드 어렵네

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -10,10 +10,5 @@ export default async function getSession() {
   return getIronSession<SessionContent>(await cookies(), {
     cookieName: "gallery_cookie",
     password: process.env.COOKIE_PASSWORD!,
-    cookieOptions: {
-      sameSite: "lax", // 크로스-사이트 요청에서 동작
-      maxAge: 60 * 60 * 24 * 7, // 7일 유지
-      path: "/", // 모든 경로에서 접근 가능
-    },
   });
 }
