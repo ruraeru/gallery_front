2f42a4f05bda37712b4b68ebf83ab3286c0aafcb
ruraeru <f93335383@gmail.com>
2025-05-10 13:57:46 +0900

    build: build err 기능 다 만들고 고치자

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  allowedDevOrigins: [
-    "http://localhost:3000", // 로컬 개발 서버
-    "https://localhost:3000", // HTTPS 로컬
-    "http://127.0.0.1:3000", // 로컬 IP
-    "https://127.0.0.1:3000", // HTTPS 로컬 IP
-    "http://172.30.1.59:3000", // 모바일 IP (예시)
-    "https://172.30.1.59:3000", // HTTPS 모바일 IP
-    "https://frontdev.kro.kr", // 외부 도메인
-    "http://frontdev.kro.kr", // HTTP 버전 (필요 시)
-  ],
+import { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: "4mb",
