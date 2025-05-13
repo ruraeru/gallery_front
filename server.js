5d3ea5dafbc56d0d11374356c43b812d19c3bd2e
ruraeru <f93335383@gmail.com>
2025-05-13 22:23:21 +0900

    deploy: ""

--- server.js
+++ server.js
@@ -1,32 +1,26 @@
 import fs from 'fs'
-import { createServer } from 'https'
-import { parse } from "url"
+import https from 'https'
 import next from 'next'
 
 const port = process.env.PORT //port
 const hostname = process.env.HOST_NAME // ip
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev, hostname, port })
-const handle = app.getRequestHandler();
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
 const httpsOptions = {
-    key: fs.readFileSync(`./${process.env.KEY}.pem`), // 생성된 개인 키 파일
-    cert: fs.readFileSync(`./${process.env.CERY_KEY}.pem`), // 생성된 인증서 파일
+    key: fs.readFileSync(process.env.KEY), // 생성된 개인 키 파일
+    cert: fs.readFileSync(process.env.CERT_KEY), // 생성된 인증서 파일
 }
 
 app.prepare().then(() => {
-    createServer(httpsOptions, async (req, res) => {
-        try {
-            const parsedUrl = parse(req.url, true);
-            await handle(req, res, parsedUrl);
-        } catch (err) {
-            console.error('Error occurred handling', req.url, err);
-            res.statusCode = 500;
-            res.end('internal server error');
-        }
-    }).listen(port, (err) => {
-        if (err) throw err;
-        console.log(`> Ready on https://${hostname}:${port}`);
-    });
-});
+    https
+        .createServer(httpsOptions, (req, res) => {
+            handle(req, res)
+        })
+        .listen(port, hostname, (err) => {
+            if (err) throw err
+            console.log(`> Ready on https://${hostname}:${port}`)
+        })
+})
