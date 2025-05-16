90d0b7397be3b037347fb1ce8c9affddd83e2c59
ruraeru <f93335383@gmail.com>
2025-05-16 22:43:10 +0900

    feat: user profile validation

--- src/app/(tabs)/users/[id]/page.tsx
+++ src/app/(tabs)/users/[id]/page.tsx
@@ -1,15 +1,51 @@
 import PostCard from "@/components/posts/PostCard";
-import { getUser } from "@/service/userService";
+import { getUser, isOwn } from "@/service/userService";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
 export default async function UserProfile({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
     const user = await getUser(id);
-    if (!user || user.id.match("admin")) {
+    const myProfile = await isOwn(id);
+    if (!user) {
         return notFound();
     }
     return (
-        <div>
+        <div style={{
+            maxWidth: "1200px",
+            margin: "0 auto"
+        }}>
+            {myProfile && <button>asdas</button>}
+            <div style={{
+
+            }}>
+                <div style={{
+                    width: "100%",
+                    height: "199px",
+                    backgroundColor: "red",
+                    background: `url(${user.posts[0]?.image}) center/cover no-repeat`,
+                }} />
+                <div style={{
+                    position: "relative"
+                }}>
+                    <div style={{
+                        width: "133px",
+                        height: "133px",
+                        background: `url(${user.avatar}) center/cover no-repeat`,
+                        borderRadius: "50%",
+                        position: "absolute",
+                        top: "50%",
+                        marginTop: "-15%",
+                        border: "4px solid black"
+                    }} />
+                    <div style={{
+                        width: "100%",
+                        height: "199px",
+                        backgroundColor: "black"
+                    }}
+                    />
+                </div>
+            </div>
             @{user.id}
             {user.username}
             <div>
--- src/service/userService.ts
+++ src/service/userService.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import db from "@/lib/db";
+import getSession from "@/lib/session";
 
 export const getUser = async (id: string) => {
   const user = await db.user.findUnique({
@@ -11,8 +12,26 @@ export const getUser = async (id: string) => {
       id: true,
       avatar: true,
       username: true,
-      posts: true,
+      posts: {
+        include: {
+          _count: true,
+        },
+      },
     },
   });
   return user;
+};
+
+export const isOwn = async (id: string) => {
+  const session = await getSession();
+  const user = await db.user.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return user?.id === session.id;
 };
