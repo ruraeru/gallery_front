53219112a5fe04493526e0eadc93d9180a73358b
ruraeru <f93335383@gmail.com>
2025-05-24 17:23:57 +0900

    fixed: font-face -> next/font/local

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import { Metadata } from "next";
 import { Suspense } from "react";
+import localFont from 'next/font/local';
 import "@/styles/globals.css";
+
+const pretendard = localFont({
+  src: "../fonts/Pretendard-Regular.ttf",
+  display: "swap",
+  weight: "400",
+  style: "normal",
+  variable: "--font-pretendard"
+})
 
 export const metadata: Metadata = {
   title: "모아통: 당신의 추억을 공유해요!",
@@ -16,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={`${pretendard.variable} ${pretendard.className}`}>
       <body>
         <Suspense>{children}</Suspense>
       </body>
