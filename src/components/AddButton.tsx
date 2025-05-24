be0902683d0c64104a9238a58292d61e7d90b768
ruraeru <f93335383@gmail.com>
2025-05-24 17:33:39 +0900

    style: postaddbutton design

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -5,9 +5,9 @@ import { PlusCircleIcon } from "@heroicons/react/24/solid";
 
 export default function AddButton() {
   return (
-    <div className="fixed bottom-6 right-6">
-      <Link href="/posts/add">
-        <PlusCircleIcon className="w-12 h-12 text-primary hover:text-primary-hover transition-colors" />
+    <div className="post-add-button">
+      <Link href="/posts/add" >
+        <PlusCircleIcon width={64} />
       </Link>
     </div>
   );
