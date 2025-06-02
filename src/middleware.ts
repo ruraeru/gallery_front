75c9f074e688008832ed1dfdb7952d6ba7a9a136
ruraeru <f93335383@gmail.com>
2025-06-03 00:19:47 +0900

    chore: 주석 제거

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,3 @@
-import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
@@ -12,9 +11,6 @@ const publicOnlyUrls: Routes = {
   "/signup": true,
 };
 
-//middleware === edge funtion
-//빠르게 실행되야하고 경량화 NodeJS API라 지원하지 않는 패키지들이 많음
-//https://nextjs.org/docs/pages/api-reference/edge
 export async function middleware(req: NextRequest) {
   const session = await getSession();
   const exists = publicOnlyUrls[req.nextUrl.pathname];
@@ -22,16 +18,9 @@ export async function middleware(req: NextRequest) {
     if (!exists) {
       return NextResponse.redirect(new URL("/", req.url));
     }
-  } else {
-    // if (exists) {
-    //   return NextResponse.redirect(new URL("/products", req.url));
-    // }
   }
-  // const cookie = await cookies();
-  // session.id === cookie.get("delicious-karrot");
 }
 
-//middleware가 실행 될 곳을 지정 가능
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)"],
 };
